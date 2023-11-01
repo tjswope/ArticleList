e74d672ef49172c00c0fb2a55f10178c94251dff
swopet <swopet@lm-swopet-tlap.lmsd.org>
2023-11-01 12:20:27 -0400

    updated main comments.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-
+// project: ArticleList
+// description: This project will make an API call to newsapi.org, parse the data and then allow
+//              the user to make execute several methods on the ArticleList.
+// written by: Mr. Swope
+// date: 11/1/2023
 
 public class Main {
 
